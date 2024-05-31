5c7936f248dbf631e4c74a5e3fbbc6c06aeb132a
stevenlee87 <imepisode1@gmail.com>
2024-05-31 10:54:48 +0800

    add some io example

---
+++ io/src/main.rs
@@ -0,0 +1,9 @@
+// learnrust/io/src/main.rs
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut output = io::stdout();
+    output.write(b"Hello, World!").unwrap();
+    output.flush().unwrap();
+}
